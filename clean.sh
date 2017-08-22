bf741f12568bbb8294f6b7eaf7dc9e6f9a266fc1
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-08-22 14:15:58 +0100

    .

--- clean.sh
+++ clean.sh
@@ -1,1 +1,2 @@
+cd 8xdrm; sh clean.sh cd ../;
 rm -f bin/* lib/*.a inc/*.h bci.o
--- compile.sh
+++ compile.sh
@@ -1,12 +1,15 @@
-cd ../8xdrm;
-sh compile.sh
+C_IFLAGS="-I8xdrm/inc -Ieint_t/inc"
+C_LFLAGS="-L8xdrm/lib"
 
-cd ../bci;
+cd 8xdrm;
+sh compile.sh -I../eint_t/inc
+
+cd ../;
 
 rm -f bci.o bin/bci
-gcc -c -I../8xdrm/inc -std=c11 -o bci.o bci.c
+gcc -c $C_IFLAGS -std=c11 -o bci.o bci.c
 
 ar rc lib/libbci.a bci.o
 cp bci.h inc
 
-gcc -Iinc -Llib -I../8xdrm/inc -L../8xdrm/lib -std=c11 -o bin/bci main.c -lbci -l8xdrm
+gcc -Iinc -Llib $C_IFLAGS $C_LFLAGS -std=c11 -o bin/bci main.c -lbci -l8xdrm
